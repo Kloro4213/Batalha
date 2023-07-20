a4bb967012b58f16d962178b93f7d1043fabde92
Kloro4213 <yelsewrsl@gmail.com>
2023-07-20 17:35:59 -0300

    coisa pouca
    fiz quase nada

--- classe_carta.py
+++ classe_carta.py
@@ -3,7 +3,59 @@ import PySimpleGUI as sg
 from colorama import Fore, Back, Style
 
 class Carta:
-    def __init__(self,nome=None,efeito=None,dono=None):
+    def __init__(self,nome=None,efeito=None,desc=None,dono=None):
         self.nome = nome
+        self.desc = desc
         self.efeito = efeito
         self.dono = dono
+
+
+
+teste = Carta( 
+                #Nome
+                "Teste",
+                
+
+                #Efeito
+                None,
+                
+
+                #Descrição
+                "TESTE ISSO É UM TESTE",
+                
+
+                #Dono??????
+                None
+            )	
+        
+					
+					
+teste = Carta( 
+                #Nome
+                "Teste",
+                
+
+                #Efeito
+                None,
+                
+
+                #Descrição
+                "TESTE ISSO É UM TESTE",
+                
+
+                #Dono??????
+                None
+            )
+					
+					
+
+					
+					
+					
+
+					
+					
+
+
+print(teste.nome)
+print(teste.desc)
